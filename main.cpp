37e3759a8b00df9b838347ce2f5324344d345465
CodingGuardian <62391363+CodingGardian@users.noreply.github.com>
2023-11-02 12:01:54 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 /* 
 	Survival Of the fittest
 	========================
-	this is a natural selection simulator that I made based off of a video I saw. Basically, you have a bunch or creatures, and these creatures compete for food on a virtual plane, you collect food, you get to live!!! you collect two food, you get to reprodice, you collect no food, you, well, die. You have a random chance to get a mutation that gives you speed, which increases speed, size, which allows you to eat other creatures, or luck, which gives you a chance to live after collecting no food. your offspring will carry your stats, and have a chance of getting stronger stats, so run the simulation, and see what you get!!!
+	this is a natural selection simulator that I made based off of a video I saw. Basically, you have a bunch or creatures, and these creatures compete for food on a virtual plane, you collect food, you get to live!!! you collect two food, you get to reproduce, you collect no food, you die. You have a random chance to get a mutation that gives you speed, which increases speed, size, which allows you to eat other creatures, or luck, which gives you a chance to live after collecting no food. your offspring will carry your stats, and have a chance of getting stronger stats, so run the simulation, and see what you get!!!
 	=========================
 */
 
